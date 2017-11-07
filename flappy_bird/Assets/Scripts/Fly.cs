b16dbaecaea51ba9e43f0e8ee58661fd8355ad60
yuanfengyun <273461474@qq.com>
2017-11-08 01:41:07 +0800

    flappy bird初版

---
+++ flappy_bird/Assets/Scripts/Fly.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour {
+    public float speed = 0.8f;
+    public int up = 1;
+    public float interval = 0;
+    public float max = 2; 
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        interval += Time.deltaTime;
+        if (interval > max)
+        {
+            interval = 0;
+            up = -up;
+        }
+
+        if (up > 0)
+        {
+            this.transform.Translate(-Vector3.up * speed * Time.deltaTime);
+            
+        }
+        else
+        {
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+	}
+}
---
+++ flappy_bird/Assets/Scripts/Ground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+    public float speed = 1.0f;
+    public float left = -0.46f;
+    public float right = 0.74f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if(this.transform.position.x < left)
+        {
+            Vector3 v = this.transform.position;
+            v.x = 0.74f;
+            this.transform.position = v;
+        }
+    }
+}
